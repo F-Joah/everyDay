41ca99e8a21680fbbf93877759d290fe42007160
N_Joah <joah1996@163.com>
2019-11-05 16:49:52 +0800

    Aop-Demo

--- src/main/java/com/joah/everyday/EverydayApplication.java
+++ src/main/java/com/joah/everyday/EverydayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EverydayApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(EverydayApplication.class, args);
     }
 
--- src/main/java/com/joah/everyday/N20191104/SpringAop/HttpAopAdviseDefine.java
+++ src/main/java/com/joah/everyday/N20191104/SpringAop/HttpAopAdviseDefine.java
@@ -4,53 +4,60 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-@Aspect
 @Component
+@Aspect
 public class HttpAopAdviseDefine {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     /**
-     * 定义一个切点,使用，切点表达式函数  来描述对那些 join Point 使用 advise
+     * 定义一个 Pointcut, 使用 切点表达式函数 来描述对哪些 Join point 使用 advise.
      */
     @Pointcut("@annotation(com.joah.everyday.N20191104.SpringAop.AuthChecker)")
-    public void pointcut(){
-
+    public void pointcut() {
     }
 
+    /**
+     * 定义 advise
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("pointcut()")
-    public Object checkAuth(ProceedingJoinPoint joinPoint)throws Throwable{
-
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest();
 
         /**
-         * 检查用户的token是否正确
+         * 检查用户所传递的 token 是否合法
          */
         String token = getUserToken(request);
-        if (!"123456".equals(token)){
-            return "错误，权限不合法";
+        if (!token.equalsIgnoreCase("123456")){
+            return "错误, 权限不合法!";
         }
 
         return joinPoint.proceed();
     }
 
-    private String getUserToken(HttpServletRequest request){
-
+    private String getUserToken(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-        if (cookies == null)
-        {
+        if (cookies == null) {
             return "";
         }
-
-        return null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equalsIgnoreCase("user_token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
     }
-
 }
