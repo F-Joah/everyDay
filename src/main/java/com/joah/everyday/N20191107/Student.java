adc58a991c776a90163997f61f3f05d6cdb05caf
fancy <Joah1996@163.com>
2019-11-07 22:47:03 +0800

    你猜我今天学啥了。。。

---
+++ src/main/java/com/joah/everyday/N20191107/Student.java
@@ -0,0 +1,30 @@
+package com.joah.everyday.N20191107;
+
+import lombok.Data;
+
+@Data
+public class Student implements Comparable<Student>{
+
+    private Integer id;
+
+    private Integer age;
+
+    private String name;
+
+    public Student(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    /**
+     * 按照年龄排序
+     * @param o
+     * @return
+     */
+    @Override
+    public int compareTo(Student o) {
+
+        return this.age - o.age;
+    }
+}
