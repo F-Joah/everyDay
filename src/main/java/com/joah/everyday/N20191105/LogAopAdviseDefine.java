7288b36823e96ff3e1453dd3ad7b049629b03360
N_Joah <joah1996@163.com>
2019-11-08 19:33:53 +0800

    Log - Aop 修复

--- src/main/java/com/joah/everyday/N20191105/LogAopAdviseDefine.java
+++ src/main/java/com/joah/everyday/N20191105/LogAopAdviseDefine.java
@@ -1,7 +1,7 @@
 package com.joah.everyday.N20191105;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -20,6 +20,35 @@ public class LogAopAdviseDefine {
     @Pointcut("within(NeedLogService)")
     public void pointCut(){
 
+    }
+
+    /**
+     * 它在一个符合要求的 joinpoint 方法调用前执行, 打印调用的方法名和调用的参数
+     * @param joinPoint
+     */
+    @Before("pointCut()")
+    public void logMethodInvokeParm(JoinPoint joinPoint){
+        logger.info("----Before method{} invoke, param :{} ----",joinPoint.getSignature().toShortString(),joinPoint.getArgs());
+    }
+
+    /**
+     * 这个 advice 会在方法调用成功后打印出方法名还反的参数.
+     * @param joinPoint
+     * @param retVal
+     */
+    @AfterReturning(pointcut = "pointCut()", returning = "retVal")
+    public void logMethodInvokeResult(JoinPoint joinPoint,Object retVal){
+        logger.info("----After method{} invoke, result :{} ----",joinPoint.getSignature().toShortString(),joinPoint.getArgs());
+    }
+
+    /**
+     * 这个 advice 会在指定的 joinpoint 抛出异常时执行, 打印异常的信息.
+     * @param joinPoint
+     * @param exception
+     */
+    @AfterThrowing(pointcut = "pointCut()", throwing = "exception")
+    public void logMethodInvokeException(JoinPoint joinPoint,Exception exception){
+        logger.info("---- method{} invoke, exception :{} ----",joinPoint.getSignature().toShortString(),exception.getMessage());
     }
 
 }
