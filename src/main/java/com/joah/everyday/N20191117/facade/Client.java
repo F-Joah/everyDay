69c24431410c57c9850c0f26fefe70ab6abc76b4
N_Joah <joah1996@163.com>
2019-11-17 19:33:35 +0800

    facade

---
+++ src/main/java/com/joah/everyday/N20191117/facade/Client.java
@@ -0,0 +1,20 @@
+package com.joah.everyday.N20191117.facade;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // 实例化组件
+        Light light = new Light();
+        Television television = new Television();
+        AirCondition airCondition = new AirCondition();
+        Screen screen = new Screen();
+
+        WatchTvSwtichFacade watchTvSwtichFacade = new WatchTvSwtichFacade(light,airCondition,screen,television);
+
+        watchTvSwtichFacade.on();
+        System.out.println("-------------------------------");
+        watchTvSwtichFacade.off();
+        System.out.println("-------------------------------");
+
+    }
+}
---
+++ src/main/java/com/joah/everyday/N20191117/facade/Screen.java
@@ -0,0 +1,13 @@
+package com.joah.everyday.N20191117.facade;
+
+public class Screen {
+
+    public void up(){
+        System.out.println("银幕打开了...");
+    }
+
+    public void down(){
+        System.out.println("银幕关闭了... ");
+    }
+
+}
