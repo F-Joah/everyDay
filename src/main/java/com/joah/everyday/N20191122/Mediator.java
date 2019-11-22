189dac92a7acde07aed41a8619362696eaffc44e
N_Joah <joah1996@163.com>
2019-11-22 17:01:43 +0800

    Mediator

---
+++ src/main/java/com/joah/everyday/N20191122/Mediator.java
@@ -0,0 +1,12 @@
+package com.joah.everyday.N20191122;
+
+public abstract class Mediator {
+
+    /**
+     * 申明一个联络方法
+     * @param message
+     * @param person
+     */
+    public abstract void constant(String message, Person person);
+
+}
