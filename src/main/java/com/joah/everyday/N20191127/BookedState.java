6498f7918d20f1175f448d07df84b9f70b184180
N_Joah <joah1996@163.com>
2019-11-27 20:25:42 +0800

    State

--- src/main/java/com/joah/everyday/N20191127/BookedState.java
+++ src/main/java/com/joah/everyday/N20191127/BookedState.java
@@ -1,6 +1,12 @@
 package com.joah.everyday.N20191127;
 
 public class BookedState implements State {
+
+    Room hotelManagement;
+
+    public BookedState(Room hotelManagement){
+        this.hotelManagement = hotelManagement;
+    }
     @Override
     public void bookRoom() {
 
--- src/main/java/com/joah/everyday/N20191127/CheckInState.java
+++ src/main/java/com/joah/everyday/N20191127/CheckInState.java
@@ -1,23 +1,32 @@
 package com.joah.everyday.N20191127;
 
 public class CheckInState implements State {
+
+    Room hotelManagement;
+
+    public CheckInState(Room hotelManagement){
+        this.hotelManagement = hotelManagement;
+    }
+
     @Override
     public void bookRoom() {
-
+        System.out.println("该房间已经入住了");
     }
 
     @Override
     public void unsubscribeRoom() {
-
+        System.out.println("该房间已经入住了");
     }
 
     @Override
     public void checkInRoom() {
-
+        System.out.println("退房成功...");
+        // 修改退房状态
+        hotelManagement.setState(hotelManagement.getFreeTimeState());
     }
 
     @Override
     public void checkOutRoom() {
-
+        // 不需要做操作
     }
 }
