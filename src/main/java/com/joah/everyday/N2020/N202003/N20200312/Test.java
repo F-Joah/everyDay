18dedaf22a14cf6f33a247cb1966c92af78259b2
N_Joah <joah1996@163.com>
2020-03-12 20:03:06 +0800

    fix

---
+++ src/main/java/com/joah/everyday/N2020/N202003/N20200312/Test.java
@@ -0,0 +1,27 @@
+package com.joah.everyday.N2020.N202003.N20200312;
+
+/**
+ * @author Joah
+ * @time 2020/3/12 10:04
+ */
+public class Test {
+
+    /**
+     * https://www.bilibili.com/video/av93824064?t=82&p=2
+     * 2020年3月12日10:05:05 开始
+     */
+
+    /**
+     *
+     * CAT 大众点评
+     * ELK 日志监控
+     *
+     *      Cloud Hoxton.SR1
+     *      boot 2.2.2.RELEASE
+     *      cloud alibaba 2.1.0.RELEASE
+     *      java jdk8
+     *      Meven 3.5
+     *      MySql 5.7
+     *
+     */
+}
