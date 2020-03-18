253bbcade61af3386194ffa30bf0a7e00e1c6ccf
N_Joah <joah1996@163.com>
2020-03-18 20:13:32 +0800

    cas

---
+++ N202003/src/main/java/com/neo/mianshi/N20200317/CASDemo.java
@@ -0,0 +1,17 @@
+package com.neo.mianshi.N20200317;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Joah
+ * @time 2020/3/17 21:19
+ */
+public class CASDemo {
+
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+        System.out.println(atomicInteger.compareAndSet(5, 2020) + "\t current data: " + atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5, 1024) + "\t current data: " + atomicInteger.get());
+
+    }
+}
---
+++ N202003/src/main/java/com/neo/mianshi/Test.java
@@ -0,0 +1,9 @@
+package com.neo.mianshi;
+
+/**
+ * @author Joah
+ * @time 2020/3/17 21:18
+ */
+public class Test {
+
+}
